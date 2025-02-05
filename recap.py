6c2903eff2282676ee0f8afcf680eec9879eb804
paraclete12 <oliverparaclete@gmail.com>
2025-02-05 08:33:08 +0100

    first commit

---
+++ recap.py
@@ -0,0 +1,18 @@
+# print('hello world')
+# a = int(input('input first number'))
+# b = int(input('Input second number'))
+# c = a + b *
+# 
+
+#control structures. else, if and elif, functions, import modules, array, string, dictionary */
+
+def add(a, b):
+    print(a+b)
+
+
+add(5,6)
+
+def substract(a, b):
+    print(a-b)
+
+substract(6,2) 
