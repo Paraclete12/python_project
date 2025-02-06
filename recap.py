a3326da4934f821844bbc16d1e78be9b2f115833
paraclete12 <oliverparaclete@gmail.com>
2025-02-06 16:10:56 +0100

    added variable.py

--- recap.py
+++ recap.py
@@ -16,3 +16,11 @@ def substract(a, b):
     print(a-b)
 
 substract(6,2) 
+
+#variables on python is a container that holds data */
+
+x = 10  # x is a variable holding an integer
+name = "Alice"  # name is a variable holding a string
+pi = 3.14  # pi is a variable holding a float (decimal number)
+
+print('x, name, pi')
---
+++ variables.py
@@ -0,0 +1,7 @@
+# variables
+
+x = 10  # x is a variable holding an integer
+name = "Alice"  # name is a variable holding a string
+pi = 3.14  # pi is a variable holding a float (decimal number)
+
+print(x,name,pi)
